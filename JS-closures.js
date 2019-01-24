186f2470b2529f3ed35608b8f3e54054d6e923f0
Konstantinos Koukakis <kkoukakis@windowslive.com>
2019-01-24 13:50:06 +0200

    JS-Closure Examples
    special multiply ( get 2 numbers and multiplies them)
    guessing Game (gets an amount of times)

---
+++ JS-closures.js
@@ -0,0 +1,53 @@
+
+function specialMultiply(a,b){
+    if(b !== undefined){
+        return a * b;
+    }
+     return function inner(b){
+            return a * b;
+    }
+}
+
+console.log("--------Special Multiply-------------");
+console.log(specialMultiply(3,4)); // 12
+console.log(specialMultiply(3)(4)); // 12
+console.log(specialMultiply(3)); // function(){}....
+
+console.log("---------Guessing Game---------------");
+
+function guessingGame(amount){
+    var answer = Math.floor(Math.random()*11);
+    var guesses = 0;
+    var completed = false;
+    return function(guess){
+        if(!completed){
+            guesses++
+            if(guess === answer) {
+                completed = true;
+                return "You got it!";
+            }
+            else if(guesses === amount) {
+                completed = true;
+                return "No more guesses the answer was " + answer;
+            }
+            else if(guess > answer) return "Your guess is too high!";
+            else if(guess < answer) return "Your guess is too low!";
+        }
+        return "You are all done playing!";
+    }
+}
+
+var game = guessingGame(5);
+console.log(game(1)); // "You're too low!"
+console.log(game(8)); // "You're too high!"
+console.log(game(5)); // "You're too low!"
+console.log(game(7)); // "You got it!"
+console.log(game(1)); // "You are all done playing!"
+
+var game2 = guessingGame(3);
+console.log(game2(5)); // "You're too low!"
+console.log(game2(3)); // "You're too low!"
+console.log(game2(1)); // "No more guesses the answer was 0"
+console.log(game2(1)); // "You are all done playing!"
+
+console.log("-------------------------------------");
